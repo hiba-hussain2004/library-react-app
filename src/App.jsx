ab48515a72b254884a41435f54f3e7a1efcd3450
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-09 18:06:58 +0530

    AddBook Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddBook from './components/AddBook'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddBook/>
     </>
   )
 }
